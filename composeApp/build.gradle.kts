f3707ecb4cb146c6d76f5709ef97d6941e7f1929
diegobarrena <dieegogo1234@gmail.com>
2026-05-12 15:13:49 -0600

    Increment version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -85,8 +85,8 @@ android {
         applicationId = "com.lasso.lassoapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.4.0"
     }
     packaging {
         resources {
